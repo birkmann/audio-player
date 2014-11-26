8d87b244aae5ee26c834a6a19f998d9baeed1ced
birkmann <rafkafu@yahoo.de>
2014-11-26 23:08:12 +0100

    Waveform canvas added.

--- js/main.js
+++ js/main.js
@@ -16,3 +16,19 @@ function stop() {
 function rewind() {
 	audio.currentTime = 0;
 }
+
+audio.addEventListener("timeupdate",function(){
+	document.querySelector('.current-time').innerHTML =  Math.floor(audio.currentTime);
+});
+
+document.querySelector('.duration').innerHTML = Math.floor(audio.duration);
+
+document.querySelector('.waveform').addEventListener('click', function(e) {
+	getCursorPosition(document.querySelector('.waveform'), event);
+});
+
+function getCursorPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+}
